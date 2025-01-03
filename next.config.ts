2a32f517cf378f3b6ba24f8f46f955a80c8e3f24
맥북 <maegbug@maegbug-ui-MacBookAir.local>
2025-01-04 00:54:31 +0600

    and

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['vodfgqixtogeqggmxkvz.supabase.co'], // Добавьте ваш домен здесь
+  },
 };
 
 export default nextConfig;
